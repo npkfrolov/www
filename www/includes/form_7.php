79707c3167ce8c637401d04f0e2a26368b7ace1c
npkfrolov <npkfrolov@gmail.com>
2018-04-05 16:38:06 +0300

    init repo

---
+++ www/includes/form_7.php
@@ -0,0 +1,22 @@
+<?php	
+	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0)
+	{
+		return false;
+	}
+	
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	
+	$to = 'admin@histgeo.ru'; // Email submissions are sent to this email
+
+	// Create email	
+	$email_subject = "Message from a Blocs website.";
+	$email_body = "You have received a new message. \n\n".
+				  "Name: $name \nEmail: $email \n";
+	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
+	$headers .= "From: subscription_site@histgeo.ru\n";
+	$headers .= "Reply-To: $email";	
+	
+	mail($to,$email_subject,$email_body,$headers); // Post message
+	return true;			
+?>
